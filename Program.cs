ba1a7dffc8fef9122917f7fe8d988a19da568b23
elifklyc <75589606+elifklyc@users.noreply.github.com>
2022-04-22 15:43:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SiberG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            Console.WriteLine("Lütfen ip adresinizi girin: ");
+            a = Console.ReadLine();
+            Process process = new Process();
+            // Configure the process using the StartInfo properties.
+            process.StartInfo.FileName = "nmap.exe";
+            process.StartInfo.Arguments = "nmap -T4 -A -v " + a;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            process.Start();
+            process.WaitForExit();// Waits here for the process to exit.
+
+        }
+    }
+}
